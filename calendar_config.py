b6b2852773944cf1db3f7b771b67ca604e30a1ab
Alex Shen <alex.shen@smartnews.com>
2023-08-10 16:56:20 +0800

    initial version that can work

--- calendar_config.py
+++ calendar_config.py
@@ -1,9 +1,9 @@
 SCOPE = 'https://www.googleapis.com/auth/calendar'
 USER_AGENT = 'GoogleCalendarDisplay/1.0'
-CLIENT_ID = <your calendar ID>
-CLIENT_SECRET = <your client secret>
-DEVELOPER_KEY = <your developer key>
+CLIENT_ID = 'xx'
+CLIENT_SECRET = 'xx'
+DEVELOPER_KEY = 'xx'
 CALENDAR_IDS = {
-    <room ID 1>: <Google calendar ID 1>,
-    <room ID 2>: <Google calendar ID 2>
+    'Yoda': 'xx'
 }
+EXCLUSIONS = ['some_calendar_id_1', 'some_calendar_id_2']
--- server.py
+++ server.py
@@ -1,15 +1,9 @@
-from apiclient.discovery import build
-from oauth2client.client import OAuth2WebServerFlow
-
-import httplib2
-
 import gflags
 import httplib2
-
+from oauth2client import tools
 from apiclient.discovery import build
 from oauth2client.file import Storage
 from oauth2client.client import OAuth2WebServerFlow
-from oauth2client.tools import run
 
 from datetime import datetime
 from datetime import timedelta
@@ -18,13 +12,11 @@ import dateutil.parser
 import sys
 import os
 import json
-
+import calendar_config
 from flask import Flask
 from flask import render_template
+
 app = Flask(__name__)
-
-import calendar_config
-
 FLAGS = gflags.FLAGS
 
 # had to install:
@@ -53,7 +45,7 @@ FLOW = OAuth2WebServerFlow(
 storage = Storage('calendar.dat')
 credentials = storage.get()
 if credentials is None or credentials.invalid == True:
-  credentials = run(FLOW, storage)
+    credentials = tools.run_flow(FLOW, storage)
 
 # Create an httplib2.Http object to handle our HTTP requests and authorize it
 # with our good Credentials.
@@ -64,9 +56,11 @@ http = credentials.authorize(http)
 # the Google Developers Console
 # to get a developerKey for your own application.
 service = build(serviceName='calendar', version='v3', http=http,
-       developerKey=calendar_config.DEVELOPER_KEY)
+                developerKey=calendar_config.DEVELOPER_KEY)
 
-la = pytz.timezone("America/Los_Angeles")
+la = pytz.timezone("Asia/Shanghai")
+# la = pytz.timezone("America/Los_Angeles")
+
 
 def create_time_string(dt):
     if not dt:
@@ -80,9 +74,10 @@ def create_time_string(dt):
     if minutes == 1:
         m = 'minute'
     if hours == 0:
-       return '%s %s' % (minutes, m)
+        return '%s %s' % (minutes, m)
     else:
         return '%s %s and %s %s' % (hours, h, minutes, m)
+
 
 # This method has a very sub-optimal approach to time zones.
 @app.route('/calendars')
@@ -108,9 +103,9 @@ def calendars():
     with open('calendars.json', mode='w') as calendar_file:
         json.dump({value: key for key, value in calendars.items()}, calendar_file)
 
-    free_busy = service.freebusy().query(body={"timeMin": start_time.isoformat(), 
-        "timeMax": end_time.isoformat(), 
-        "items":items}).execute()
+    free_busy = service.freebusy().query(body={"timeMin": start_time.isoformat(),
+                                               "timeMax": end_time.isoformat(),
+                                               "items": items}).execute()
 
     for calendar in free_busy['calendars']:
         data = free_busy['calendars'][calendar]
@@ -120,8 +115,8 @@ def calendars():
             end = dateutil.parser.parse(busy['end']) - timedelta(hours=8)
             diff = start - (now - timedelta(hours=16))
 
-            event = {'room': calendars[calendar], 
-                     'start': start.strftime("%l:%M%p"), 
+            event = {'room': calendars[calendar],
+                     'start': start.strftime("%l:%M%p"),
                      'end': end.strftime("%l:%M%p")}
 
             if diff < timedelta(minutes=5):
@@ -131,24 +126,51 @@ def calendars():
                 upcoming.append(event)
                 free_rooms.remove(calendar)
 
-    return render_template('calendars.html', 
-                           events=events, 
+    return render_template('calendars.html',
+                           events=events,
                            upcoming=upcoming,
                            now=start_time.strftime("%A %e %B %Y, %l:%M%p"),
                            free_rooms=[calendars[f] for f in free_rooms])
 
+@app.route('/user/<user_email>')
+def user(user_email):
+    # 获取 user_name 对应的 Google 日历会议信息
+    meetings = get_user_meetings(user_email)
+
+    # 渲染 user.html 模板，并传递 meetings 列表
+    return render_template('user.html', meetings=meetings, user_name=user_email)
+
+
+def get_user_meetings(user_email):
+    # 根据 user_name 获取与该用户关联的 Google 日历 ID 或其他标识符
+    calendar_id = user_email
+
+    # 使用 Google 日历 API 获取会议信息
+    events_result = service.events().list(
+        calendarId=calendar_id,
+        orderBy='startTime',
+        singleEvents=True
+    ).execute()
+
+    # 从 API 响应中提取会议信息
+    all_meetings = events_result.get('items', [])
+    meetings_by_creator = [event for event in all_meetings if event['creator']['email'] == 'hugo.gomez@smartnews.com']
+
+    return meetings_by_creator
+
+
 def get_events(room_name):
     items = []
-    now = datetime.utcnow()
-
-    la_offset = la.utcoffset(datetime.utcnow())
-    now = now + la_offset
-
+    shanghai_tz = pytz.timezone("Asia/Shanghai")
+    now = datetime.utcnow().replace(tzinfo=pytz.utc)  # 创建一个 UTC 时间对象
+    now = now.astimezone(shanghai_tz)  # 将其转换为上海时区
+    print(now.strftime("%Y-%m-%d %H:%M:%S"))
     start_time = datetime(year=now.year, month=now.month, day=now.day, tzinfo=la)
+    print('开始时间：' + str(start_time))
     end_time = start_time + timedelta(days=1)
 
-    print "Running at", now.strftime("%A %e %B %Y, %l:%M%p")
-    print "Room name", room_name
+    print("Running at", now.strftime("%A %e %B %Y, %l:%M%p"))
+    print("Room name", room_name)
 
     if not os.path.isfile('calendars.json'):
         # this is duplicated from the calendars() method
@@ -168,11 +190,11 @@ def get_events(room_name):
     room_id = calendars[room_name]
 
     events = service.events().list(
-    	calendarId=room_id,
-    	orderBy='startTime',
-    	singleEvents=True,
-    	timeMin=start_time.isoformat(),
-    	timeMax=end_time.isoformat()
+        calendarId=room_id,
+        orderBy='startTime',
+        singleEvents=True,
+        timeMin=start_time.isoformat(),
+        timeMax=end_time.isoformat()
     ).execute()
 
     next_start = None
@@ -180,16 +202,21 @@ def get_events(room_name):
     status = "FREE"
 
     for event in events['items']:
-        start = dateutil.parser.parse(event['start']['dateTime']).replace(tzinfo=None)
-        end = dateutil.parser.parse(event['end']['dateTime']).replace(tzinfo=None)
-
+        start = dateutil.parser.parse(event['start']['dateTime'])
+        end = dateutil.parser.parse(event['end']['dateTime'])
+        start = start.astimezone(now.tzinfo)
+        end = end.astimezone(now.tzinfo)
+        if event['creator'].get('displayName'):
+            event_creator = event['creator']['displayName']
+        else:
+            event_creator = event['creator']['email']
         if now <= end:
-            items.append({'name': event['summary'], 
-                'creator': event['creator']['displayName'], 
-                'start': start.strftime("%l:%M%p"), 
-                'end': end.strftime("%l:%M%p"),
-                })
- 
+            items.append({'name': event['summary'],
+                          'creator': event_creator,
+                          'start': start.strftime("%l:%M%p"),
+                          'end': end.strftime("%l:%M%p"),
+                          })
+
             if start < now and end > now:
                 status = "BUSY"
                 next_end = end - now
@@ -197,36 +224,38 @@ def get_events(room_name):
             if start > now and not next_start:
                 next_start = start - now
 
-
     next_start_str = create_time_string(next_start)
     next_end_str = create_time_string(next_end)
 
     if status == "FREE" and next_start and next_start < timedelta(minutes=15):
         status = "SOON"
 
-    return {'room': events['summary'], 
-        'status': status, 
-        'now': now.strftime("%A %e %B %Y, %l:%M%p"), 
-        'events': items, 
-        'next_start_str': next_start_str, 
-        'next_end_str': next_end_str}
+    return {'room': events['summary'],
+            'status': status,
+            'now': now.strftime("%A %e %B %Y, %l:%M%p"),
+            'events': items,
+            'next_start_str': next_start_str,
+            'next_end_str': next_end_str}
+
 
 @app.route('/index/<room_name>')
 def index(room_name=None):
     events = get_events(room_name)
 
-    return render_template('index.html', 
-        status=events['status'], 
-        events=events['events'], 
-        next_start_str=events['next_start_str'], 
-        next_end_str=events['next_end_str'], 
-        now=events['now'],
-        room=room_name
-    )
+    return render_template('index.html',
+                           status=events['status'],
+                           events=events['events'],
+                           next_start_str=events['next_start_str'],
+                           next_end_str=events['next_end_str'],
+                           now=events['now'],
+                           room=room_name
+                           )
+
 
 @app.route('/<room_id>')
 def main(room_id):
-  return render_template('main.html', room=room_id)
+    return render_template('main.html', room=room_id)
+
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", debug=True)
+    app.run(host="0.0.0.0", port=5001, debug=True)
